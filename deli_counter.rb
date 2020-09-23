36f71114c28b1d4446bb79608cd9fdae18a47c34
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-23 01:57:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = []
 
-def line(katz_deli_line) 
-  if katz_deli_line.length == 0 
+def line(katz_deli_line)
+  if katz_deli_line.length == 0
     "The line is currently empty."
-  end 
+  end
 end 
