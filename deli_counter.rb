3e3f7823c5d609f1caff67bdbe3fa42c20465740
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-23 02:25:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,14 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+  elsif katz.deli.length > 0
+    written_line = []
+    number_in_line = 1
+    katz_deli.each do |patron|
+        written_line << "#{number_in_line}. #{patron}
+        number_in_line += 1
+    end
+    puts "The line is currently: " << written_line.join(" ")
   end
 end
 
