30cea09915399a3631a4a80a77b39f4037a2c0fb
Carolyn Manning <carolyn.manning3@gmail.com>
2020-09-23 02:10:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,5 +7,5 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "#{name} #{katz_deli[katz_deli.length]}"
+  puts "Welcome, #{name}. You are number #{katz_deli[katz_deli.length]}"
 end
